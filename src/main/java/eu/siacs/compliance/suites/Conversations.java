f65750e80b05142817546168d1af4e37760bf815
Daniel Gultsch <daniel@gultsch.de>
2016-06-06 13:39:32 +0200

    added extra test to check for prosodys cloud notify

--- src/main/java/eu/siacs/compliance/suites/Conversations.java
+++ src/main/java/eu/siacs/compliance/suites/Conversations.java
@@ -20,7 +20,7 @@ public class Conversations extends AdvancedServerIM {
                 CSI.class,
                 HttpUpload.class,
                 Proxy65.class,
-                Push.class
+                PushOnServer.class
         ).stream()).collect(Collectors.toList());
     }
 
