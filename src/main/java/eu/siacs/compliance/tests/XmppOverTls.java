7a349c97da253ad481768e9712fb38c5270f408f
Daniel Gultsch <daniel@gultsch.de>
2017-01-24 14:55:39 +0100

    set sni hostname for for xep-0368 + actual cert verification

--- src/main/java/eu/siacs/compliance/tests/XmppOverTls.java
+++ src/main/java/eu/siacs/compliance/tests/XmppOverTls.java
@@ -4,11 +4,15 @@ import de.measite.minidns.hla.ResolverApi;
 import de.measite.minidns.hla.ResolverResult;
 import de.measite.minidns.record.SRV;
 import eu.siacs.compliance.Result;
+import eu.siacs.utils.XmppDomainVerifier;
 import rocks.xmpp.core.session.XmppClient;
 
+import javax.net.ssl.SNIHostName;
+import javax.net.ssl.SSLParameters;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class XmppOverTls extends AbstractTest {
 
@@ -18,22 +22,32 @@ public class XmppOverTls extends AbstractTest {
 
     @Override
     public Result run() {
-        String domain = client.getDomain().getDomain();
+        final String domain = client.getDomain().getDomain();
+        final SSLParameters parameters = new SSLParameters();
+        parameters.setServerNames(Arrays.asList(new SNIHostName(domain)));
         try {
             final SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
             ResolverResult<SRV> results = ResolverApi.INSTANCE.resolve("_xmpps-client._tcp." + domain, SRV.class);
             for(SRV record : results.getAnswers()) {
                 try {
                     SSLSocket socket = (SSLSocket) sslSocketFactory.createSocket(record.name.toString(),record.port);
+                    socket.setSSLParameters(parameters);
                     socket.setSoTimeout(1000);
                     socket.startHandshake();
+                    final Result result;
+                    if (XmppDomainVerifier.getInstance().verify(domain,socket.getSession())) {
+                        result = Result.PASSED;
+                    } else {
+                        result = Result.FAILED;
+                    }
                     socket.close();
-                    return Result.PASSED;
+                    return result;
                 } catch (IOException e) {
                     //ignored
                 }
             }
         } catch (Exception e) {
+            e.printStackTrace();
             return Result.FAILED;
         }
         return Result.FAILED;
