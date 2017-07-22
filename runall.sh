d29a91540d8e1d323ec8f5b73dae26ea4af4ebd2
Daniel Gultsch <daniel@gultsch.de>
2017-07-22 20:15:47 +0200

    modified runall script to execute jobs in parallel

--- runall.sh
+++ runall.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+function pwait() {
+    while [ $(jobs -p | wc -l) -ge $1 ]; do
+        sleep 1
+    done
+}
 for account in `grep entry accounts.xml | awk -F '"' '{print $2}'`; do
   server=$(echo $account | awk -F '@' '{print $2}')
   echo "testing $server"
-  java -jar target/ComplianceTester-0.1.jar $account > reports/$server.txt
+  java -jar target/ComplianceTester-0.1.jar $account > reports/$server.txt&
+  pwait 5
 done;
+
