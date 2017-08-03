fbc6ca989df367cb8deb68ab6f9e963cdaac3a4b
Daniel Gultsch <daniel@gultsch.de>
2017-08-03 18:48:52 +0200

    add javadoc comment to OMEMO test

--- src/main/java/eu/siacs/compliance/tests/OMEMO.java
+++ src/main/java/eu/siacs/compliance/tests/OMEMO.java
@@ -7,9 +7,14 @@ import rocks.xmpp.extensions.disco.model.info.Identity;
 import rocks.xmpp.extensions.disco.model.info.InfoNode;
 import rocks.xmpp.util.concurrent.AsyncResult;
 
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * This test checks for the availability of publish-options on the account’s PEP service.
+ * publish-options allows a client to efficiently change the access model of the OMEMO key material
+ * such that everyone can access it. Without publish-options OMEMO is only available to contacts with
+ * mutual presence subscription. 
+ */
 public class OMEMO extends AbstractTest {
 
     public OMEMO(XmppClient client) {
@@ -25,7 +30,7 @@ public class OMEMO extends AbstractTest {
             if (!infoNode.getFeatures().contains("http://jabber.org/protocol/pubsub#publish-options")) {
                 return Result.FAILED;
             }
-            for(Identity identity : infoNode.getIdentities()) {
+            for (Identity identity : infoNode.getIdentities()) {
                 if ("pep".equalsIgnoreCase(identity.getType()) && "pubsub".equalsIgnoreCase(identity.getCategory())) {
                     return Result.PASSED;
                 }
