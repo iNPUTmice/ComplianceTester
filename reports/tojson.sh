5001c273f63d0dfbd3a6c6e22b84b66f0dc7ea6d
Daniel Gultsch <daniel@gultsch.de>
2017-08-23 17:18:26 +0200

    fixed tojson script to work with more tests

--- reports/tojson.sh
+++ reports/tojson.sh
@@ -6,7 +6,7 @@ echo -n '{'
 for report in "${reports[@]}"; do
   server=$(echo $report | sed 's/.txt//')
   echo -n "\"$server\":"
-  lines=`grep -B 16 'Conversations Compliance Suite: ' $report | grep running | wc -l`
+  lines=`grep -B 17 'Conversations Compliance Suite: ' $report | grep running | wc -l`
   context=`echo $lines + 2 | bc`
   echo -n '{' ; grep -B $context 'Conversations Compliance Suite: ' $report | head -n $lines | sed 's/running //' | sed 's/…//' | sed -e 's/ \{3,\}/\t\t/g' | awk -v lines="$lines" -F '\t\t' '{ printf "\"" $1 "\":\""  $2 "\""; if (NR != lines) printf "," }'; echo -n '}'
   if [[ $report != $last ]]; then echo -n ','; fi;
